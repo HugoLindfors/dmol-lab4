5ec9d09b9d815918eb2de98688dd07a1ac727c5a
Hugo Lindfors <HugoLindfors@users.noreply.github.com>
2025-10-23 18:27:40 +0200

    fixed M_1

--- M_1.m
+++ M_1.m
@@ -1,22 +1,12 @@
 % M_1 is intended to imitate a finite automation from a lecture that accepts all strings with an even number of 1's and reject everything else.
-function a = M_1(str)
+function current_state = M_1(input_string)
 
-% Always clear the command window.
-clc;
+START_STATE = true;
 
-% delta defines the movement rules: for 1, q₁ -> q₂ and q₂ -> q₁, for 0, q₁ -> q₁ and q₂ -> q₂.
-delta = [ 1, 2; 2, 1; ];
+current_state = START_STATE;
 
-% F defines accepted out values. We require the final state to be q₁ because then there are an even number of 1's.
-F = [ 1, 0 ];
-
-% This is our start state a.k.a. q₁.
-j = 1;
-
-for i = 1 : length(str)
-  s = str2num(string(i)); %#ok<ST2NM>
-  j = delta(j, s);
+for i = 1 : length(input_string)
+  symbol = str2double(input_string(i));
+  if symbol; current_state = ~current_state; end
 end
-
-a = F(j) == 1;
 end
